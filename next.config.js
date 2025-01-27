03ae470393e45e96acb84f8bec669b4dcf880f46
Graham Grieve <grahamgrieve@Grahams-MacBook-Air.local>
2025-01-27 15:50:18 -0500

    Add configuration files for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Pathway',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
